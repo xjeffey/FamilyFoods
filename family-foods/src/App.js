dfebcd788dc035c92cd6e227311e1ca0c098bcc9
xjeffey <tranjeff@live.com>
2022-08-23 18:07:45 -0400

    test

--- family-foods/src/App.js
+++ family-foods/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Family Foods</h1>
 
-      {/* Header */}
+      <Header></Header>
 
       {/* App Body */}
         {/* Side Bar */}
--- family-foods/src/Components/Header/Header.js
+++ family-foods/src/Components/Header/Header.js
@@ -8,9 +8,17 @@ function Header() {
         <h1>Family Circle Name</h1>
       </div>
 
-      <div className="middle"></div>
+      <div className="middle">
+        <h1>Memories</h1>
+        <h1>Recipes</h1>
+        <h1>Requests</h1>
+      </div>
 
-      <div className="right"></div>
+      <div className="right">
+        <h1>Members Here</h1>
+        <h1>My Icon</h1>
+        <h1>Button</h1>
+      </div>
     </div>
   )
 }
