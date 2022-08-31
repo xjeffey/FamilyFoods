b84cd21543907deb6311519a724f57325c056575
xjeffey <tranjeff@live.com>
2022-08-30 23:42:31 -0400

    working on nav bar

--- family-foods/src/App.js
+++ family-foods/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Header from './Components/Header/Header';
+import Home from './Pages/Home';
 import Memories from './Pages/Memories';
 import Recipes from './Pages/Recipes';
 import Requests from './Pages/Requests';
@@ -8,21 +9,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Header />
+    <div className="app">
+      <Header />
 
-        <Routes>
-          <Route exact path="/Memories" element={<Memories/>}></Route>
-          <Route exact path="/Recipes" element={<Recipes/>}></Route>
-          <Route exact path="/Requests" element={<Requests/>}></Route>
-        </Routes>
-        {/* App Body */}
-          {/* Side Bar */}
-          {/* Feed */}
+      {/* App Body */}
+      <div className="app__body">
+        {/* Side Bar */}
+        {/* Feed */}
       </div>
-    </Router>
-    
+    </div>  
   );
 }
 
--- family-foods/src/Components/Header/Header.js
+++ family-foods/src/Components/Header/Header.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Header.css";
-import { AccountCircle, PersonAdd } from '@mui/icons-material';
+import { AccountCircle, PersonAdd, MenuBook, Home, Feed, Flag } from '@mui/icons-material';
 import { Button, IconButton } from '@mui/material';
 
 function Header() {
   return (
     <div className='header'>
-      <div className="header-left">
+      <div className="header__left">
         <h1>Family Circle Name</h1>
       </div>
 
-      <div className="header-middle">
-        <Link className='nav-link' to="/Memories"><h1>Memories</h1></Link>
-        <Link className='nav-link' to="/Recipes"><h1>Recipes</h1></Link>
-        <Link className='nav-link' to="/Requests"><h1>Requests</h1></Link>
+      <div className="header__middle">
+        <div className='header__navLink header__navLink--active'>
+          <Home fontSize='large' />
+        </div>
+        <div className='header__navLink'>
+          <MenuBook fontSize='large' />
+        </div>
+        <div className='header__navLink'>
+          <Feed fontSize='large' />
+        </div>
+        <div className='header__navLink'>
+          <Flag fontSize='large' />
+        </div>
       </div>
 
-      <div className="header-right">
-        <IconButton>
-          <AccountCircle fontSize='large' />
-        </IconButton>
-        <Button variant="contained" startIcon={<PersonAdd />}>Invite</Button>
+      <div className="header__right">
+        <div className="header__profile">
+          <IconButton>
+            <AccountCircle fontSize='large' />
+          </IconButton>
+        </div>
+    
+        <div className="header__invite">
+           <Button size="large" variant="contained" startIcon={<PersonAdd />}>Invite</Button>
+        </div>
       </div>
     </div>
   )
